f1b7d5daa490b308f5fe3144b710540f70645a2f
Greg <gregrs@fastmail.co.uk>
2019-07-30 11:54:48 +0100

    Improve decision method differences script
    Decision methods now get flaps from self.box. Corrections using pylint.

--- decision_method_difference.py
+++ decision_method_difference.py
@@ -2,32 +2,35 @@
 
 """
 Output the differences between the flap decision methods
-make_flap_decision_highest and make_flap_decision_next_roll_probability.
+make_flap_decision_highest and make_flap_decision_next_roll_probability
+for combinations of flaps with length 4-9 i.e. always summing to more
+than 6.
 """
 
-import shutthebox
 import itertools
+import shutthebox
 
-box = shutthebox.Box()
-dice = shutthebox.Dice()
+DICE = shutthebox.Dice()
 
-turn = shutthebox.ComputerTurn(box, dice)
-
+# 4-9 flaps i.e. always summing to more than 6
 for length in range(9, 4 - 1, -1):
     for this_combination in itertools.combinations(range(1, 9 + 1), length):
-        for this_dice_sum in turn.dice_sum_probabilities.keys():
-            highest = turn.make_flap_decision_highest(
-                this_combination,
-                this_dice_sum,
-                turn.make_num_dice_decision_one_if_poss)
-            if highest == False: break
-            highest.sort()
+        for this_dice_sum in range(2, 12 + 1):
+            # prepare instance with particular flaps raised
+            box = shutthebox.Box()
+            turn = shutthebox.ComputerTurn(box, DICE)
+            turn.box.lower_flaps_except(this_combination)
 
-            next_roll_prob = turn.make_flap_decision_next_roll_probability(
-                this_combination,
+            highest = turn.make_flap_decision_highest(
                 this_dice_sum,
                 turn.make_num_dice_decision_one_if_poss)
-            if next_roll_prob == False: break
+            if highest is False: # not possible to lower any flaps
+                break
+            next_roll_prob = turn.make_flap_decision_next_roll_probability(
+                this_dice_sum,
+                turn.make_num_dice_decision_one_if_poss)
+
+            highest.sort()
             next_roll_prob.sort()
 
             if highest != next_roll_prob:
